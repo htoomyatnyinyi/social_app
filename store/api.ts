d5db3b3284b20e314f57a5487876fc64439472b9
htoomyatnyinyi <htoomyatnyinyi@gmail.com>
2026-02-11 11:18:24 +0700

    update post delete , report and block

--- store/api.ts
+++ store/api.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// export const API_URL = "http://localhost:8080"; // Replace with your IP for physical devices
+export const API_URL = "http://localhost:8080"; // Replace with your IP for physical devices
 // export const API_URL = "http://192.168.1.245:8080"; // Replace with your IP for physical devices
-export const API_URL = "http://192.168.1.144:8080"; // Replace with your IP for physical devices
+// export const API_URL = "http://192.168.1.144:8080"; // Replace with your IP for physical devices
 
 export const api = createApi({
   reducerPath: "api",
